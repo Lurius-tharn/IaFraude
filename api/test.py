0b663f6b9aac07cf1cc0bee2e0a154bc99b93feb
Kesu19 <74417754+Kesu19@users.noreply.github.com>
2023-10-10 11:38:35 +0200

    TESTE moi ca BG

--- api/test.py
+++ api/test.py
@@ -1,8 +1,43 @@
+import pickle
 from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 app = FastAPI()
 
+# Chargez le modèle depuis le fichier .pkl
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Classe Pydantic pour représenter les données d'entrée
+class InputData(BaseModel):
+    distance_from_home: float
+    distance_from_last_transaction: float
+    ratio_to_median_purchase_price: float
+    repeat_retailer: int
+    used_chip: int
+    online_order: int
+
+# Endpoint pour effectuer des prédictions
+@app.post("/predict/")
+def predict(data: InputData):
+    # Créez un DataFrame à partir des données d'entrée
+    input_data = pd.DataFrame({
+        "distance_from_home": [data.distance_from_home],
+        "distance_from_last_transaction": [data.distance_from_last_transaction],
+        "ratio_to_median_purchase_price": [data.ratio_to_median_purchase_price],
+        "repeat_retailer": [data.repeat_retailer],
+        "used_chip": [data.used_chip],
+        "online_order": [data.online_order]
+    })
+
+    # Mettez à l'échelle les données d'entrée avec le même scaler utilisé lors de l'entraînement
+    scaler = StandardScaler()
+    scaled_data = scaler.transform(input_data)
+
+    # Effectuez la prédiction
+    prediction = model.predict(scaled_data)
+
+    # Renvoyez les prédictions sous forme de réponse
+    return {"prediction": prediction[0]}
