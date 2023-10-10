c9d253750c1733129201f81e69157362a77158d4
Davenese <sajous.davenese@gmail.com>
2023-10-10 12:28:12 +0200

    on a le front et le back

--- web/frontend/src/app/app.module.ts
+++ web/frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {HttpClientModule} from "@angular/common/http";
 	imports: [
 		BrowserModule,
 		FormsModule,
-     HttpClientModule
+     HttpClientModule,
+     NgbModule
 	],
   providers: [],
   bootstrap: [AppComponent]
