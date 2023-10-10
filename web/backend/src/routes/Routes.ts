7fa9349d6ff100bbb7b0a2a56174147fa83f1339
Davenese <sajous.davenese@gmail.com>
2023-10-10 10:11:37 +0200

    xonfig projet

---
+++ web/backend/src/routes/Routes.ts
@@ -0,0 +1,29 @@
+import {NextFunction, Request, Response} from "express";
+import {FraudeController} from "../controller/FraudeController";
+
+
+export class Routes {
+
+
+	private FraudeController : FraudeController
+	constructor () {
+		this.FraudeController = new FraudeController()
+	}
+
+	public routes(app) : void{
+		app.route('/')
+			.get((request: Request, response: Response) => {
+				response.status(200)
+					.send({
+						message: "GET request successfully."
+					});
+			});
+		app.route("/fraudes").get((request: Request, response: Response, next: NextFunction) => this.FraudeController.testFraude(request, response, next));
+
+
+	}
+
+
+}
+
+
